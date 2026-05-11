1bb8ab80b7e8fbb373888a09e415f33bafeae886
gahbyu <gahbyu@inha.edu>
2026-05-12 00:50:16 +0900

    setting/#1: 디자인 시스템 정의(폰트, 색, 아이콘, 이미지)

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/theme/Type.kt
@@ -2,33 +2,48 @@ package com.example.a38_collaboration_android_kakaopay.core.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.a38_collaboration_android_kakaopay.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val NotoSansKR = FontFamily(
+    Font(R.font.noto_sans_kr_bold, FontWeight.Bold),
+    Font(R.font.noto_sans_kr_medium, FontWeight.Medium),
+    Font(R.font.noto_sans_kr_regular, FontWeight.Normal)
 )
+
+fun getTextStyle(
+    fontWeight: FontWeight,
+    fontSize: Int,
+    lineHeightPercent: Double = 1.4,
+    letterSpacingPercent: Double = -0.01
+): TextStyle = TextStyle(
+    fontFamily = NotoSansKR,
+    fontWeight = fontWeight,
+    fontSize = fontSize.sp,
+    lineHeight = (fontSize * lineHeightPercent).sp,
+    letterSpacing = (fontSize * letterSpacingPercent).sp
+)
+
+val TitleB26 = getTextStyle(FontWeight.Bold, 26)
+val TitleB24 = getTextStyle(FontWeight.Bold, 24)
+val TitleB20 = getTextStyle(FontWeight.Bold, 20)
+val TitleB18 = getTextStyle(FontWeight.Bold, 18)
+val TitleM18 = getTextStyle(FontWeight.Medium, 18)
+
+val BodyB16 = getTextStyle(FontWeight.Bold, 16)
+val BodyM16 = getTextStyle(FontWeight.Medium, 16)
+val BodyB14 = getTextStyle(FontWeight.Bold, 14)
+val BodyM14 = getTextStyle(FontWeight.Medium, 14)
+val BodyR14 = getTextStyle(FontWeight.Normal, 14)
+
+val LabelB12 = getTextStyle(FontWeight.Bold, 12)
+val LabelM12 = getTextStyle(FontWeight.Medium, 12)
+val LabelR12 = getTextStyle(FontWeight.Normal, 12)
+val LabelR10 = getTextStyle(FontWeight.Normal, 10)
+
+
+
+
