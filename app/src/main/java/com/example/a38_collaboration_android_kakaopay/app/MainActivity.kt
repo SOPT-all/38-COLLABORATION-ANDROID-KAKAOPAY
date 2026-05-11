76ea8841bfb894127f95ec095f7b4cb4f620fea7
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-12 01:59:33 +0900

    setting/#4: 프로젝트 구조 설정

---
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a38_collaboration_android_kakaopay.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme._38COLLABORATIONANDROIDKAKAOPAYTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            _38COLLABORATIONANDROIDKAKAOPAYTheme {
+                App()
+            }
+        }
+    }
+}
