322e321bd0c2974fd41ac1b7eb22142996f67967
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-13 21:00:26 +0900

    feature/#9: noRippleClickableWithPressedColor 구현
    - 선택되었을 때 PressedColor로 선택되도록 구현

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/common/extension/Modifier.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/common/extension/Modifier.kt
@@ -1,20 +1,51 @@
 package com.example.a38_collaboration_android_kakaopay.core.common.extension
 
+
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme
 
-@Composable
+
 inline fun Modifier.noRippleClickable(
-    crossinline onClick: () -> Unit
+    crossinline onClick: () -> Unit,
 ): Modifier = composed {
+
     this.clickable(
         indication = null,
         interactionSource = remember { MutableInteractionSource() }
     ) {
         onClick()
     }
+}
+
+inline fun Modifier.noRippleClickableWithPressedColor(
+    pressedColor: Color? = null,
+    crossinline onClick: () -> Unit,
+): Modifier = composed {
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val pressedColor = pressedColor ?: KakaoTheme.colors.buttonSecondaryPressed
+
+    this
+        .then(
+            if (isPressed) {
+                Modifier.background(color = pressedColor)
+            } else {
+                Modifier
+            }
+        )
+        .clickable(
+            indication = null,
+            interactionSource = interactionSource
+        ) {
+            onClick()
+        }
 }
