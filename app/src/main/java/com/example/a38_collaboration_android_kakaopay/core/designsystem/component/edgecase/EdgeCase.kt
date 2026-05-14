d7ae4833ab59841c1425323580f19fca7a2be99e
yeseul <joyri0202@gmail.com>
2026-05-14 21:50:29 +0900

    refactor/#12: 레이아웃 간격 설정을 Spacer에서 spacedBy로 변경

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/edgecase/EdgeCase.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/edgecase/EdgeCase.kt
@@ -1,9 +1,8 @@
 package com.example.a38_collaboration_android_kakaopay.core.designsystem.component.edgecase
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,15 +24,13 @@ fun EdgeCase(
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(25.dp)
     ) {
         Image(
             painter = painterResource(type.icon),
             contentDescription = null,
             modifier = Modifier.size(64.dp),
         )
-
-        Spacer(modifier = Modifier.height(25.dp))
-
         Text(
             text = stringResource(type.text),
             color = colors.black,
