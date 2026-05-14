6bdbe844073404ae01fd12254e295f1f45245809
yeseul <joyri0202@gmail.com>
2026-05-14 21:54:52 +0900

    refactor/#29: 레이아웃 간격 설정을 Spacer에서 spacedBy로 변경

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/transaction/TransactionDropdown.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/transaction/TransactionDropdown.kt
@@ -1,10 +1,9 @@
 package com.example.a38_collaboration_android_kakaopay.core.designsystem.component.transaction
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,6 +34,7 @@ fun TransactionDropdown(
                 horizontal = 6.dp
             ),
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(2.dp)
     ){
         Text(
             text = "전체",
@@ -42,9 +42,6 @@ fun TransactionDropdown(
             style = typography.bodyB16,
             textAlign = TextAlign.Center
         )
-
-        Spacer(modifier = Modifier.width(2.dp))
-
         Icon(
             imageVector = ImageVector.vectorResource(R.drawable.ic_chevron_down_black_16px),
             contentDescription = null,
