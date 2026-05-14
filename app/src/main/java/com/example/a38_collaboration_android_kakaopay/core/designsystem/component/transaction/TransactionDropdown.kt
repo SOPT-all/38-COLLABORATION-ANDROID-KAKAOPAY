41c182447df3dd6f8d37c11483960bcca02dde17
yeseul <joyri0202@gmail.com>
2026-05-14 21:35:36 +0900

    feature/#29: TransactionDropdown 컴포넌트 구현

---
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/transaction/TransactionDropdown.kt
@@ -0,0 +1,68 @@
+package com.example.a38_collaboration_android_kakaopay.core.designsystem.component.transaction
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a38_collaboration_android_kakaopay.R
+import com.example.a38_collaboration_android_kakaopay.core.common.extension.noRippleClickable
+import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoPayTheme
+import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme.colors
+import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme.typography
+
+@Composable
+fun TransactionDropdown(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .noRippleClickable(onClick)
+            .padding(
+                vertical = 2.dp,
+                horizontal = 6.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        Text(
+            text = "전체",
+            color = colors.black,
+            style = typography.bodyB16,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.width(2.dp))
+
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_chevron_down_black_16px),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TransactionDropdownPreview() {
+    KakaoPayTheme {
+        Box(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            TransactionDropdown(
+                onClick = {},
+            )
+        }
+    }
+}
