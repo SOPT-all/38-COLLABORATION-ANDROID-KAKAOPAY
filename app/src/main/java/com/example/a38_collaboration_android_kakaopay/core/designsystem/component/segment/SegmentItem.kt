fd5ced5d3caeaf94ff1b5881c105082e638dde9f
yeseul <joyri0202@gmail.com>
2026-05-15 01:59:46 +0900

    feature/#17: noRippleClickableWithPressedColor 적용

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/segment/SegmentItem.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/segment/SegmentItem.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.a38_collaboration_android_kakaopay.core.common.extension.noRippleClickable
+import com.example.a38_collaboration_android_kakaopay.core.common.extension.noRippleClickableWithPressedColor
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoPayTheme
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme.colors
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme.typography
@@ -29,6 +29,7 @@ fun SegmentItem(
     modifier: Modifier = Modifier,
 ) {
     val backgroundColor = if (isSelected) colors.segmentBackgroundSelected else colors.white
+    val pressedColor = if (isSelected) backgroundColor else colors.buttonSecondaryPressed
     val borderColor = if (isSelected) Color.Transparent else colors.grey200
     val textColor = if (isSelected) colors.white else colors.grey500
     val textStyle = if (isSelected) typography.bodyB14 else typography.bodyR14
@@ -42,7 +43,10 @@ fun SegmentItem(
                 color = borderColor,
                 shape = RoundedCornerShape(6.dp),
             )
-            .noRippleClickable(onClick)
+            .noRippleClickableWithPressedColor(
+                pressedColor,
+                onClick = onClick
+            )
             .padding(
                 vertical = 6.dp,
                 horizontal = 12.dp,
@@ -66,7 +70,7 @@ private fun SegmentItemPreview() {
         Column(
             modifier = Modifier.padding(20.dp),
             verticalArrangement = Arrangement.spacedBy(10.dp)
-        ){
+        ) {
             SegmentItem(
                 text = "전체",
                 isSelected = true,
