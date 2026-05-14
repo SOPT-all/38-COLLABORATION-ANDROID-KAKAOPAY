5e292290e2839cdf6f191faedda389f4334cf028
yeseul <joyri0202@gmail.com>
2026-05-15 02:32:37 +0900

    feature/#29: icon tint color 설정

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/dropdown/TransactionDropdown.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/dropdown/TransactionDropdown.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -49,7 +48,7 @@ fun TransactionDropdown(
         Icon(
             imageVector = ImageVector.vectorResource(R.drawable.ic_chevron_down_black_16px),
             contentDescription = null,
-            tint = Color.Unspecified
+            tint = colors.black
         )
     }
 }
