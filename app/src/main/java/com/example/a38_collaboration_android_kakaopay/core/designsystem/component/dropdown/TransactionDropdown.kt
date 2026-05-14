05f8a6f932c74f6c7415987e87149488ebd163d7
yeseul <joyri0202@gmail.com>
2026-05-15 02:27:21 +0900

    feature/#29: noRippleClickableWithPressedColor 적용

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/dropdown/TransactionDropdown.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/dropdown/TransactionDropdown.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -17,7 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a38_collaboration_android_kakaopay.R
-import com.example.a38_collaboration_android_kakaopay.core.common.extension.noRippleClickable
+import com.example.a38_collaboration_android_kakaopay.core.common.extension.noRippleClickableWithPressedColor
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoPayTheme
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme.colors
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme.typography
@@ -29,7 +31,8 @@ fun TransactionDropdown(
 ) {
     Row(
         modifier = modifier
-            .noRippleClickable(onClick)
+            .clip(RoundedCornerShape(4.dp))
+            .noRippleClickableWithPressedColor(onClick = onClick)
             .padding(
                 vertical = 2.dp,
                 horizontal = 6.dp
