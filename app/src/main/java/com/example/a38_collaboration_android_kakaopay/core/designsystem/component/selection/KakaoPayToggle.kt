4f9f4ea4a9e9c760cdc15e1f676d42f981674232
xancxoxin <judy010321@naver.com>
2026-05-16 07:53:22 +0900

    fix/#35: 코드 리뷰 반영

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/selection/KakaoPayToggle.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/selection/KakaoPayToggle.kt
@@ -3,11 +3,10 @@ package com.example.a38_collaboration_android_kakaopay.core.designsystem.compone
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -21,31 +20,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.a38_collaboration_android_kakaopay.core.common.extension.noRippleClickable
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoPayTheme
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme
-
-@Composable
-fun KakaoPayToggle(
-    modifier: Modifier = Modifier,
-    initialChecked: Boolean = false,
-    onCheckedChange: (Boolean) -> Unit = {}
-) {
-    val isChecked = remember { mutableStateOf(initialChecked) }
-
-    KakaoPayToggle(
-        checked = isChecked.value,
-        onCheckedChange = { newValue ->
-            isChecked.value = newValue
-            onCheckedChange(newValue)
-        },
-        modifier = modifier
-    )
-}
 
 @Composable
 fun KakaoPayToggle(
@@ -68,45 +48,46 @@ fun KakaoPayToggle(
         modifier = modifier
             .width(48.dp)
             .height(31.dp)
-            .clip(RoundedCornerShape(99.dp))
-            .background(backgroundColor)
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-            ) {
-                onCheckedChange(!checked)
-            },
+            .background(backgroundColor, RoundedCornerShape(99.dp))
+            .noRippleClickable { onCheckedChange(!checked) },
         contentAlignment = Alignment.CenterStart
     ) {
         // 토글 핸들
         Box(
             modifier = Modifier
-                .padding(start = 2.dp)
+                .padding(horizontal = 2.dp)
                 .offset(x = thumbOffset)
                 .size(27.dp)
                 .shadow(
                     elevation = 2.dp,
                     shape = CircleShape,
-                    spotColor = KakaoTheme.colors.black.copy(alpha = 0.2f), // #00000033 반영
-                    ambientColor = KakaoTheme.colors.black.copy(alpha = 0.2f)
+                    clip = false
                 )
-                .clip(CircleShape)
-                .background(KakaoTheme.colors.white)
+                .background(KakaoTheme.colors.white, CircleShape)
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun KakaoPayTogglePreview() {
+private fun KakaoPayTogglePreview() {
     KakaoPayTheme {
-        val testChecked = remember { mutableStateOf(false) }
+        val isChecked = remember { mutableStateOf(false) }
 
-        Column {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
             KakaoPayToggle(
-                initialChecked = false,
-                onCheckedChange = { isChecked ->
+                checked = isChecked.value,
+                onCheckedChange = { newValue ->
+                    isChecked.value = newValue
                 }
             )
-        }
+
+                    }
     }
 }
