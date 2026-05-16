87f8203c03faf105e3e58ccc18cdde6298ab3ddf
xancxoxin <judy010321@naver.com>
2026-05-17 02:08:32 +0900

    fix/#20: 불필요한 import 제거

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/topbar/KakaoPaySubTopBar.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/topbar/KakaoPaySubTopBar.kt
@@ -2,14 +2,11 @@ package com.example.a38_collaboration_android_kakaopay.core.designsystem.compone
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
