c6c149a7e1c10b0308dbcff706936ba4fc438bc5
xancxoxin <judy010321@naver.com>
2026-05-16 19:04:33 +0900

    fix/#37: 불필요한 변수 제거 및 onclick 호출 방식 변경

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/button/KakaoPayScrollTopButton.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/button/KakaoPayScrollTopButton.kt
@@ -2,8 +2,6 @@ package com.example.a38_collaboration_android_kakaopay.core.designsystem.compone
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,8 +14,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,15 +32,12 @@ fun KakaoPayScrollTopButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
-    val isPressed by interactionSource.collectIsPressedAsState()
-
     Box(
         modifier = modifier
             .size(48.dp)
             .background(KakaoTheme.colors.white, CircleShape)
             .border(1.dp, KakaoTheme.colors.grey200, CircleShape)
-            .noRippleClickableWithPressedColor{ onClick() },
+            .noRippleClickableWithPressedColor(onClick = onClick),
         contentAlignment = Alignment.Center
     ) {
         Icon(
