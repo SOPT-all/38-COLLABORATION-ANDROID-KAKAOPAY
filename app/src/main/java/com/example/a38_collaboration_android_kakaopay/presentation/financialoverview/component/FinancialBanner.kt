95fefa58930c2a9b2bc4f4e07507a30c98898562
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-17 19:54:53 +0900

    feature/#41 : FinancialBanner 구현 -

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/financialoverview/component/FinancialBanner.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/financialoverview/component/FinancialBanner.kt
@@ -1,16 +1,21 @@
 package com.example.a38_collaboration_android_kakaopay.presentation.financialoverview.component
 
 
-import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,10 +29,48 @@ fun FinancialBanner(
     modifier: Modifier = Modifier,
 ) {
     KakaoPayBasicCard(
-        modifier = modifier,
-
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 14.dp),
+            horizontalArrangement = Arrangement.spacedBy(44.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
 
+            FinancialSubTitleGroup()
+
+            Image(
+                painter = painterResource(R.drawable.img_3d_03),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun FinancialSubTitleGroup(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start,
+    ) {
+
+        Text(
+            text = stringResource( R.string.financial_banner_subtitle),
+            color = KakaoTheme.colors.grey500,
+            style = KakaoTheme.typography.bodyR14
+        )
+
+        Text(
+            text = stringResource(R.string.financial_banner_title),
+            modifier = Modifier.padding(bottom = 14.dp),
+            color = KakaoTheme.colors.black,
+            style = KakaoTheme.typography.titleB18
+        )
+
+        CtaInlineButton()
     }
 }
 
@@ -39,8 +82,7 @@ private fun CtaInlineButton(
         modifier = modifier
             .clip(RoundedCornerShape(999.dp))
             .background(KakaoTheme.colors.brandPrimaryYellow)
-            .padding(horizontal = 12.dp, vertical = 6.dp),
-        contentAlignment = Alignment.Center
+            .padding(horizontal = 12.dp, vertical = 6.dp), contentAlignment = Alignment.Center
     ) {
         Text(
             text = stringResource(R.string.cta_inline_button),
@@ -54,7 +96,6 @@ private fun CtaInlineButton(
 @Composable
 private fun FinancialBannerPreview() {
     KakaoPayTheme {
-//        FinancialBanner()
-        CtaInlineButton()
+        FinancialBanner()
     }
 }
