ce7ed2fe876db1f4d73ec97a4f9f6ccb8f226637
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-17 20:54:03 +0900

    fix/#41 : KakaoPayOutlineButton typo 수정 bodyB16 -> bodyR14 -

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/button/KakaoPayOutlineButton.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/core/designsystem/component/button/KakaoPayOutlineButton.kt
@@ -44,7 +44,7 @@ fun KakaoPayOutlineButton(
         Text(
             text = text,
             color = KakaoTheme.colors.black,
-            style = KakaoTheme.typography.bodyB16
+            style = KakaoTheme.typography.bodyR14
         )
     }
 
