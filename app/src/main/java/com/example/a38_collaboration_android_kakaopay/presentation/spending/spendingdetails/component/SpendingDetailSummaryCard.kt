a3177a8878aa076597affa22bee096caace1c266
xancxoxin <judy010321@naver.com>
2026-05-19 00:00:08 +0900

    fix/#47: 구분선 strings.xml로 분리

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingdetails/component/SpendingDetailSummaryCard.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingdetails/component/SpendingDetailSummaryCard.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a38_collaboration_android_kakaopay.R
@@ -36,7 +37,7 @@ private fun SpendingInfoRow(
     ) {
         Text(text = label, color = KakaoTheme.colors.grey500, style = KakaoTheme.typography.bodyR14)
         Text(
-            text = "|",
+            text = stringResource(id = R.string.word_divider),
             modifier = Modifier.padding(horizontal = 8.dp),
             color = KakaoTheme.colors.grey200,
             style = KakaoTheme.typography.bodyR14
