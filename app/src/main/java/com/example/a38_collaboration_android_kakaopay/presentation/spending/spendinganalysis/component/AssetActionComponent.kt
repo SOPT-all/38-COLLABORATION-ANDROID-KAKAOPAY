915cfb3b9a90a782f450a9262ad605db748e066f
gahbyu <gahbyu@inha.edu>
2026-05-19 12:10:06 +0900

    feature/#56: expense_list 컴포넌트 구현

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendinganalysis/component/AssetActionComponent.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendinganalysis/component/AssetActionComponent.kt
@@ -1,4 +1,4 @@
-package com.example.a38_collaboration_android_kakaopay.presentation.spending.spendinganalysis
+package com.example.a38_collaboration_android_kakaopay.presentation.spending.spendinganalysis.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
