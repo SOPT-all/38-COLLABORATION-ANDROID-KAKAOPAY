81584c5684b84367b10e58404a2ef3ae1e1e6a6a
xancxoxin <judy010321@naver.com>
2026-05-19 21:02:08 +0900

    fix/#50: 안내글 row 처리

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingdetails/component/SpendingInfoCard.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingdetails/component/SpendingInfoCard.kt
@@ -6,12 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -72,11 +72,23 @@ fun SpendingInfoCard(
                 .height(2.dp)
         )
 
-        Text(
-            text = stringResource(R.string.spending_detail_info_footer_notice),
-            color = KakaoTheme.colors.grey500,
-            style = KakaoTheme.typography.bodyR14
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = stringResource(R.string.spending_detail_info_footer_icon), // 💡 가운뎃점 리소스
+                color = KakaoTheme.colors.grey500,
+                style = KakaoTheme.typography.bodyR14
+            )
+
+            Spacer(
+                modifier = Modifier.width(5.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.spending_detail_info_footer_notice),
+                color = KakaoTheme.colors.grey500,
+                style = KakaoTheme.typography.bodyR14
+            )
+        }
     }
 }
 
