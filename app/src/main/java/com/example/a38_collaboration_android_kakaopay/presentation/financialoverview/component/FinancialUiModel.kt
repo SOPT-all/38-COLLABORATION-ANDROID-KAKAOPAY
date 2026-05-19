81e151485161c7d4375890db60e249983ded6a36
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-19 13:55:14 +0900

    chore/#48 : 코드 정리

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/financialoverview/component/FinancialUiModel.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/financialoverview/component/FinancialUiModel.kt
@@ -1,6 +1,7 @@
 package com.example.a38_collaboration_android_kakaopay.presentation.financialoverview.component
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 data class FinancialList(
     @param:DrawableRes
