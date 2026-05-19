9a6be2dd592aa238ab8368cc50ab844db76d1f7f
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-20 01:40:55 +0900

    feature/#68 : Financial 스크린 적용

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/financialoverview/FinancialOverviewScreen.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/financialoverview/FinancialOverviewScreen.kt
@@ -1,24 +1,54 @@
 package com.example.a38_collaboration_android_kakaopay.presentation.financialoverview
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.a38_collaboration_android_kakaopay.core.common.state.UiState
+import com.example.a38_collaboration_android_kakaopay.presentation.financialoverview.model.FinancialOverviewUiModel
 
 @Composable
 fun FinancialOverviewRoute(
     paddingValues: PaddingValues,
     navController: NavController,
+    viewModel: FinancialOverviewViewModel = viewModel(),
 ) {
     FinancialOverviewScreen(
-        paddingValues = paddingValues
+        paddingValues = paddingValues,
+        uiState = viewModel.uiState,
     )
 }
 
 @Composable
 fun FinancialOverviewScreen(
     paddingValues: PaddingValues,
+    uiState: UiState<FinancialOverviewUiModel>,
     modifier: Modifier = Modifier,
 ) {
 
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        when (val currentState = uiState) {
+            UiState.Empty -> Unit
+            UiState.Loading -> Text(text = "Loading assets...")
+            UiState.Failure -> Text(text = "Failed to load assets")
+            is UiState.Success -> {
+                Text(text = "Balance: ${currentState.data.kakaopayBalance}")
+                Text(text = "Total expense: ${currentState.data.totalExpense}")
+                Text(text = "Favorite accounts: ${currentState.data.favoriteAccounts.size}")
+            }
+        }
+    }
 }
