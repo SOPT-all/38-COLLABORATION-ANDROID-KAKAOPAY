55b6c4dcf512fe50b1cd15abe59e628510d41ab4
xancxoxin <judy010321@naver.com>
2026-05-20 02:05:59 +0900

    fix/#39: listOf -> persistentListOf 수정

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingoverview/component/TransactionGroup.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingoverview/component/TransactionGroup.kt
@@ -17,6 +17,7 @@ import com.example.a38_collaboration_android_kakaopay.domain.model.spendingoverv
 import com.example.a38_collaboration_android_kakaopay.domain.model.spendingoverview.transaction.TransactionMethod
 import com.example.a38_collaboration_android_kakaopay.domain.model.spendingoverview.transaction.TransactionType
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun TransactionGroup(
@@ -74,7 +75,7 @@ private fun TransactionGroupPreview() {
                 date = "2026-04-23",
                 dayOfWeek = "목",
                 dailyTotal = -3475,
-                transactions = listOf(
+                transactions = persistentListOf(
                     Transaction(
                         transactionId = 13,
                         transactionType = TransactionType.TRANSFER_SEND,
