6ec23edf0c13bb3672d2f53764953dab32ebaee0
yeseul <joyri0202@gmail.com>
2026-05-20 17:13:54 +0900

    feature/#76: SpendingOverviewViewModel 선언

---
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingoverview/SpendingOverviewViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.a38_collaboration_android_kakaopay.presentation.spending.spendingoverview
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a38_collaboration_android_kakaopay.core.common.state.UiState
+import com.example.a38_collaboration_android_kakaopay.data.mapper.toUiModel
+import com.example.a38_collaboration_android_kakaopay.data.remote.RetrofitClient
+import com.example.a38_collaboration_android_kakaopay.data.remote.datasource.ExpenseDataSource
+import com.example.a38_collaboration_android_kakaopay.presentation.spending.spendingoverview.model.SpendingOverviewUiModel
+import kotlinx.coroutines.launch
+
+class SpendingOverviewViewModel : ViewModel() {
+    var uiState by mutableStateOf<UiState<SpendingOverviewUiModel>>(UiState.Loading)
+        private set
+
+    init {
+        getTransactionsList()
+    }
+
+    fun getTransactionsList() {
+        viewModelScope.launch {
+            uiState = UiState.Loading
+            runCatching {
+                ExpenseDataSource(RetrofitClient.expenseApi)
+                    .getExpenses()
+                    .data
+                    .toUiModel()
+            }.onSuccess { data ->
+                uiState = UiState.Success(data)
+            }.onFailure {
+                uiState = UiState.Failure
+            }
+        }
+    }
+}
