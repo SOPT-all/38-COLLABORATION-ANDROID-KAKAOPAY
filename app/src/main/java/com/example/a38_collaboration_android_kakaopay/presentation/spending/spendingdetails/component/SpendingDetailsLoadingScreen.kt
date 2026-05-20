90105a4de7ba51cd37f2b5a379c724e4880d8308
xancxoxin <judy010321@naver.com>
2026-05-20 21:30:37 +0900

    feature/#66: Indicator 적용

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingdetails/component/SpendingDetailsLoadingScreen.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingdetails/component/SpendingDetailsLoadingScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.a38_collaboration_android_kakaopay.core.common.util.KakaoPullToIndicator
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.component.topbar.KakaoPaySubTopBar
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoPayTheme
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.theme.KakaoTheme
@@ -47,6 +50,26 @@ fun SpendingDetailsLoadingScreen(
                 )
             }
         )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(63.dp))
+
+            Box(
+                modifier = Modifier.size(36.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                KakaoPullToIndicator(
+                    isRefreshing = true,
+                    onRefresh = {}
+                ) {
+                    Box(modifier = Modifier.size(36.dp))
+                }
+            }
+        }
     }
 }
 
