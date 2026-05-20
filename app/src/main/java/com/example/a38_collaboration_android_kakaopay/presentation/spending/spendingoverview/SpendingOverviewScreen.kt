69a21cb1561a652e3cbb5be620ff414dc03e0daa
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-05-20 23:24:50 +0900

    refactor/#70 : CTA 버튼 컴포넌트화

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingoverview/SpendingOverviewScreen.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendingoverview/SpendingOverviewScreen.kt
@@ -23,7 +23,6 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.a38_collaboration_android_kakaopay.R
 import com.example.a38_collaboration_android_kakaopay.core.common.state.UiState
-import com.example.a38_collaboration_android_kakaopay.core.common.util.KakaoPullToIndicator
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.component.binarytab.BinaryTabControl
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.component.button.KakaoPayScrollTopButton
 import com.example.a38_collaboration_android_kakaopay.core.designsystem.component.dropdown.TransactionDropdown
@@ -106,68 +105,52 @@ fun SpendingOverviewScreen(
                     .padding(horizontal = 4.dp)
             )
 
-            Box(
-                modifier = Modifier
-                    .weight(1f)
+            LazyColumn(
+                state = listState,
+                contentPadding = PaddingValues(
+                    bottom = 123.dp
+                ),
             ) {
-                LazyColumn(
-                    state = listState,
-                    contentPadding = PaddingValues(
-                        bottom = 123.dp
-                    ),
-                    modifier = Modifier,
-                ) {
-                    item {
-                        Column(
-                            modifier = Modifier,
-                        ) {
-                            Spacer(modifier = Modifier.height(24.dp))
+                item {
+                    Column(
+                        modifier = Modifier,
+                    ) {
+                        Spacer(modifier = Modifier.height(24.dp))
 
-                            OverviewSection(
-                                uiState = uiState,
-                                onMonthChanged = onMonthChanged,
-                                selectedMonth = selectedMonth,
-                                onCategoryAnalysisClick = onCategoryAnalysisClick
-                            )
+                        OverviewSection(
+                            uiState = uiState,
+                            onMonthChanged = onMonthChanged,
+                            selectedMonth = selectedMonth,
+                            onCategoryAnalysisClick = onCategoryAnalysisClick
+                        )
 
-                            if (uiState is UiState.Success) {
-                                Spacer(modifier = Modifier.height(32.dp))
+                        if (uiState is UiState.Success) {
+                            Spacer(modifier = Modifier.height(32.dp))
 
-                                TransactionHeader()
-                            }
+                            TransactionHeader()
                         }
                     }
-
-                    when (uiState) {
-                        is UiState.Success -> {
-                            uiState.data.dailyTransactions.forEach { dailyTransaction ->
-                                item {
-                                    TransactionGroup(
-                                        dailyTransactions = dailyTransaction,
-                                        onTransactionClick = onTransactionClick,
-                                        modifier = Modifier
-                                            .padding(bottom = 24.dp)
-                                    )
-                                }
-                            }
-                        }
-
-                        else -> {}
-                    }
-
                 }
 
-                if (uiState is UiState.Loading) {
-                    KakaoPullToIndicator(
-                        isRefreshing = true,
-                        onRefresh = {},
-                        modifier = Modifier
-                            .align(Alignment.TopCenter)
-                            .padding(top = 18.dp)
-                    ) { }
+                when (uiState) {
+                    is UiState.Success -> {
+                        uiState.data.dailyTransactions.forEach { dailyTransaction ->
+                            item {
+                                TransactionGroup(
+                                    dailyTransactions = dailyTransaction,
+                                    onTransactionClick = onTransactionClick,
+                                    modifier = Modifier
+                                        .padding(bottom = 24.dp)
+                                )
+                            }
+                        }
+                    }
+
+                    else -> {}
                 }
 
             }
+
         }
 
         KakaoPayScrollTopButton(
