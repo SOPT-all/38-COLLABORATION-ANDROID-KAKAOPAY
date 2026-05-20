e9bc6edacfec81b1ea58e0c2845e397998fef2f4
xancxoxin <judy010321@naver.com>
2026-05-20 17:32:04 +0900

    feature/#78: 지출 상세 내역 API 인터페이스 및 DataSource 구현
    # Conflicts:
    #	app/src/main/java/com/example/a38_collaboration_android_kakaopay/data/remote/api/ExpenseApi.kt
    #	app/src/main/java/com/example/a38_collaboration_android_kakaopay/data/remote/datasource/ExpenseDataSource.kt
    
    # Conflicts:
    #	app/src/main/java/com/example/a38_collaboration_android_kakaopay/data/remote/api/ExpenseApi.kt
    #	app/src/main/java/com/example/a38_collaboration_android_kakaopay/data/remote/datasource/ExpenseDataSource.kt

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/data/remote/api/ExpenseApi.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/data/remote/api/ExpenseApi.kt
@@ -2,20 +2,14 @@ package com.example.a38_collaboration_android_kakaopay.data.remote.api
 
 import com.example.a38_collaboration_android_kakaopay.core.network.BaseResponse
 import com.example.a38_collaboration_android_kakaopay.data.remote.dto.response.ExpenseDetailResponse
-import com.example.a38_collaboration_android_kakaopay.data.remote.dto.response.ExpenseResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
-interface ExpenseApi {
-
-    @GET("api/v1/expenses")
-    suspend fun getExpenses(
-        @Query("yearMonth") yearMonth: String
-    ): BaseResponse<ExpenseResponse>
+interface ExpenseApi{
 
     @GET("api/v1/expenses/{transactionId}")
     suspend fun getExpenseDetail(
         @Path("transactionId") transactionId: Long
     ): BaseResponse<ExpenseDetailResponse>
+
 }
