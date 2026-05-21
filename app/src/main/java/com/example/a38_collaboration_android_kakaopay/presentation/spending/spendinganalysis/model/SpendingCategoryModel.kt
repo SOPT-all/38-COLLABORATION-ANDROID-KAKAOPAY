2a852a753dfea578dd88813f8972bd49d52a3ca6
gahbyu <gahbyu@inha.edu>
2026-05-22 01:26:22 +0900

    qa/#95: 배달 이미지 추가 및 맵핑 변경

--- app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendinganalysis/model/SpendingCategoryModel.kt
+++ app/src/main/java/com/example/a38_collaboration_android_kakaopay/presentation/spending/spendinganalysis/model/SpendingCategoryModel.kt
@@ -4,7 +4,7 @@ import com.example.a38_collaboration_android_kakaopay.R
 
 enum class SpendingCategoryModel(val koreanName: String, val imageResId: Int) {
 
-    DELIVERY("배달",R.drawable.img_transport),
+    DELIVERY("배달",R.drawable.img_delivery),
     TRANSPORTATION("교통", R.drawable.img_transport),
     FOOD("음식", R.drawable.img_food),
     COFFEE_DESSERT("커피/디저트", R.drawable.img_cafe),
